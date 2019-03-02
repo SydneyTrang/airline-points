87b61bae9706857338e1fd4009426c4229a4e2a4
Sydney Trang <37758639+SydneyTrang@users.noreply.github.com>
2019-03-02 09:55:31 -0500

    Add files via upload

---
+++ Passengers.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sydney
+ */
+public class Passengers {
+
+    String sFullName, sIDNumber;
+    int iWeek1, iWeek2, iWeek3, iWeek4;
+
+    Passengers(String sName, String sIDNum, int iWk1, int iWk2, int iWk3, int iWk4) {
+        sFullName = sName;
+        sIDNumber = sIDNum;
+        iWeek1 = iWk1;
+        iWeek2 = iWk2;
+        iWeek3 = iWk3;
+        iWeek4 = iWk4;
+    }
+
+}
